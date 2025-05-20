87bba159a5cf6e63e2de573373ff9a1ad6e16fee
Sofia <sofiaamv2003@gmail.com>
2025-05-19 23:21:06 -0500

    PayMode Model

--- sisven/app/Models/PayMode.php
+++ sisven/app/Models/PayMode.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PayMode extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'observation',
+    ];
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
