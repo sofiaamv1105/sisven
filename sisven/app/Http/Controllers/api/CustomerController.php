1a33e4e46809a05aa717278a8a514d3643ebd76a
Sofia <sofiaamv2003@gmail.com>
2025-05-20 00:20:14 -0500

    Customer Controller

--- sisven/app/Http/Controllers/api/CustomerController.php
+++ sisven/app/Http/Controllers/api/CustomerController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Customer;
 
 class CustomerController extends Controller
 {
@@ -12,38 +14,63 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        $customers = DB::table('customers')->get();
+        return json_encode(['customers' => $customers]);
     }
-
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $customer = new Customer();
+        $customer->document_number = $request->document_number;
+        $customer->first_name = $request->first_name;
+        $customer->last_name = $request->last_name;
+        $customer->address = $request->address;
+        $customer->birthday = $request->birthday;
+        $customer->phone_number = $request->phone_number;
+        $customer->email = $request->email;
+        $customer->save();
+
+        return json_encode(['customer' => $customer]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $customer = Customer::find($id);
+        return json_encode(['customer' => $customer]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $customer = Customer::find($id);
+        $customer->document_number = $request->document_number;
+        $customer->first_name = $request->first_name;
+        $customer->last_name = $request->last_name;
+        $customer->address = $request->address;
+        $customer->birthday = $request->birthday;
+        $customer->phone_number = $request->phone_number;
+        $customer->email = $request->email;
+        $customer->save();
+
+        return json_encode(['customer' => $customer]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $customer = Customer::find($id);
+        $customer->delete();
+
+        $customers = DB::table('customers')->get();
+        return json_encode(['customers' => $customers, 'success' => true]);
     }
 }
