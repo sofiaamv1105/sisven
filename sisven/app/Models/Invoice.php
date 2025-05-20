2da5e3ab82d30a4bdd50e9d4e4fc02ad499ee41f
Sofia <sofiaamv2003@gmail.com>
2025-05-19 23:19:12 -0500

    Invoice Model

--- sisven/app/Models/Invoice.php
+++ sisven/app/Models/Invoice.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Invoice extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'number',
+        'customer_id',
+        'date',
+        'pay_mode_id',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function payMode()
+    {
+        return $this->belongsTo(PayMode::class);
+    }
+
+    public function details()
+    {
+        return $this->hasMany(Detail::class);
+    }
 }
