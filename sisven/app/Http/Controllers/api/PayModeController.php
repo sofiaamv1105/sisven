054675f6aed2dc19e6349db62a430f64cb303c36
Sofia <sofiaamv2003@gmail.com>
2025-05-20 00:15:37 -0500

    PayMode Controller

--- sisven/app/Http/Controllers/api/PayModeController.php
+++ sisven/app/Http/Controllers/api/PayModeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\PayMode;
 
 class PayModeController extends Controller
 {
@@ -12,7 +14,8 @@ class PayModeController extends Controller
      */
     public function index()
     {
-        //
+        $payModes = DB::table('pay_mode')->get();
+        return json_encode(['pay_modes' => $payModes]);
     }
 
     /**
@@ -20,30 +23,46 @@ class PayModeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $payMode = new PayMode();
+        $payMode->name = $request->name;
+        $payMode->observation = $request->observation;
+        $payMode->save();
+
+        return json_encode(['pay_mode' => $payMode]);
     }
+
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $payMode = PayMode::find($id);
+        return json_encode(['pay_mode' => $payMode]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $payMode = PayMode::find($id);
+        $payMode->name = $request->name;
+        $payMode->observation = $request->observation;
+        $payMode->save();
+
+        return json_encode(['pay_mode' => $payMode]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $payMode = PayMode::find($id);
+        $payMode->delete();
+
+        $payModes = DB::table('pay_mode')->get();
+        return json_encode(['pay_modes' => $payModes, 'success' => true]);
     }
 }
