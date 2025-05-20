0206957904c1932bac0560207cc3b2b98c5c0468
Sofia <sofiaamv2003@gmail.com>
2025-05-19 23:20:22 -0500

    Product Model

--- sisven/app/Models/Product.php
+++ sisven/app/Models/Product.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'stock',
+        'category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function details()
+    {
+        return $this->hasMany(Detail::class);
+    }
 }
