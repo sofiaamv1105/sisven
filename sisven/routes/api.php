93e93fd692fe9dc48c8272ba339773d506c2374b
Sofia <sofiaamv2003@gmail.com>
2025-05-20 00:13:53 -0500

    Rutas para Product

--- sisven/routes/api.php
+++ sisven/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,6 +11,12 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
 Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
-Route::delete('/categories/{catrgory}', [CategoryController::class, 'destroy'])->name('categories.destroy');
-Route::get('/categories/{catrgory}', [CategoryController::class, 'show'])->name('categories.show');
-Route::put('/categories/{catrgory}', [CategoryController::class, 'update'])->name('categories.update');
+Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
