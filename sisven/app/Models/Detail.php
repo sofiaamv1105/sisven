c1351d13fd67be2ab1c7cbec4ac0bd09c8bc3c44
Sofia <sofiaamv2003@gmail.com>
2025-05-19 23:19:42 -0500

    Detail Model

--- sisven/app/Models/Detail.php
+++ sisven/app/Models/Detail.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Detail extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'invoice_id',
+        'product_id',
+        'quantity',
+        'price',
+    ];
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
