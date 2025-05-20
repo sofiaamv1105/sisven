ad21802b73d5f8a975e14d48a0a9926602afdb9b
Sofia <sofiaamv2003@gmail.com>
2025-05-19 23:18:10 -0500

    Customer Model

--- sisven/app/Models/Customer.php
+++ sisven/app/Models/Customer.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'document_number',
+        'first_name',
+        'last_name',
+        'address',
+        'birthday',
+        'phone_number',
+        'email',
+    ];
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
